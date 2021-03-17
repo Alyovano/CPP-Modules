75ba02e1c775d38048c6401b72226d4e302503e2
coucou <alyovano@student.s19.be>
2021-03-17 18:44:21 +0100

    Indent' exo cppd01e01

--- day01/ex01/ex01.cpp
+++ day01/ex01/ex01.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ex01.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alyovano <alyovano@student.s19.be>         +#+  +:+       +#+        */
+/*   By: aly <aly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 11:55:37 by alyovano          #+#    #+#             */
-/*   Updated: 2021/03/12 11:56:26 by alyovano         ###   ########.fr       */
+/*   Updated: 2021/03/17 18:43:34 by aly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	memoryLeak()
 	delete panthere;
 }
 
-int		main()
-{
-	memoryLeak();
-}
+// int		main()
+// {
+// 	memoryLeak();
+// }
