e1cea24b0403405e35af3b4eedc6614a39cd8246
coucou <alyovano@student.s19.be>
2021-04-05 13:28:53 +0200

    Ex01 / day02 depart

---
+++ day02/ex01/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aly <aly@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/04 15:53:54 by aly               #+#    #+#             */
+/*   Updated: 2021/04/04 21:17:33 by aly              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FixedClass.hpp"
+
+int		main(void) {
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
